519420af4124a903cdc6056b55db6d79f6d3147e
dotndev <christelle.canovas@gmail.com>
2020-02-13 15:26:55 +0100

    Committing all files

---
+++ public/js/javascript.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $('.table').DataTable();
+  $('.dataTables_length').addClass('bs-select');
+});
+
+
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,8 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Doctrine\DBAL\Driver\Connection;
 
 class MainController extends AbstractController
@@ -19,22 +18,25 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+
+    public function index(PaginatorInterface $paginator, Request $request)
     {
 
-       
-      $nains = $this -> getDoctrine()-> getRepository(Nain::class) -> FindAll();
+       //Retrieve data from Nain and add pagination to it
+     $nains = $paginator -> paginate(
+        $this -> getDoctrine()-> getRepository(Nain::class) -> FindAll(),
+        $request -> query -> getInt('page',1),
+        10
+
+      );
+
+      //Retrieve data from Mine
       $mines = $this -> getDoctrine()-> getRepository(Mine::class) -> FindAll();
+      
 
-        return  $this -> render('nains/index.html.twig', array ('nains' => $nains, 'mines' => $mines));
-
-    
+        return  $this -> render('index.html.twig', array ('nains' => $nains, 'mines' => $mines));
 
     }
-
-
- 
-
 
 }
 
